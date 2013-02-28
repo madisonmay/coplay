eab010b15aa258b5f5d7672b51649e3397261cdb
madisonmay13 <madison.may@students.olin.edu>
2013-02-28 18:06:12 -0500

    Minor homepage css change

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,18 @@
 exports.landing_page = function(req, res){
     res.render("login", {title: 'CoPlay'});
-}
+};
 
 exports.login = function(req, res){
     res.send("Placeholder for login handler");
 };
 
 exports.logout = function(req, res){
-    res.render('Placeholder for logout handler');
-}
+    res.send('Placeholder for logout handler');
+};
 
 exports.settings = function(req, res){
-    res.render('Placeholder for settings page');
-}
+    res.send('Placeholder for settings page');
+};
 
 
 
