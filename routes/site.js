3ed0bc7dfa2e1c5338b3e8227c8ba4256e6d50f0
Thomas Chen <thomas.chen@students.olin.edu>
2013-03-03 00:08:41 -0500

    added some frontend

--- routes/site.js
+++ routes/site.js
@@ -1,8 +1,8 @@
-
-exports.about = function(req, res){
-    res.render("index", {title: 'CoPlay', logged_in: false});
-};
-
 exports.home = function(req, res){
     res.render("home", {title: 'CoPlay'});
+};
+
+
+exports.about = function(req, res){
+    res.render("about", {title: 'CoPlay', logged_in: false});
 };
