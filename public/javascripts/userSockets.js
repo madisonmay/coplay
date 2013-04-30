d6ec1ba08470e0a33ad8bc0e8945c19afb768178
Derek Redfern <redfern.derek@gmail.com>
2013-04-30 01:48:28 -0400

    Forgot to upload userSockets...

---
+++ public/javascripts/userSockets.js
@@ -0,0 +1,17 @@
+$(function() {
+    var socket = io.connect('http://localhost:5000');
+    socket.emit('addToStation',$('#stationID').attr('stationID'));
+    socket.on('update', function (data) {
+        $("#songName").text(data.title + ' by ' + data.artist);
+        $("#songName").attr({'name': data.title, 'artist': data.artist});
+        $("#album-art").html("");
+
+        if (data.artwork) {
+            $("#album-art").append("<img src='http://beta.grooveshark.com/static/amazonart/l" +
+                                    data.artwork + "' class='album-art'></img>")
+        } else {
+            $("#album-art").html("<img src='http://beta.grooveshark.com/static/albums/500_album.png" +
+                                 "' class='album-art'></img>");
+        }
+    });
+});
