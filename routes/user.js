4f726a781f3201b44f4b1b8596d51ace69bd9237
Derek Redfern <redfern.derek@gmail.com>
2013-05-12 15:49:58 -0400

    Silly async methods

--- routes/user.js
+++ routes/user.js
@@ -482,7 +482,7 @@ function get_friends(fb_id, thisID, req, res, callback){
                 friend_list.push(db_user);
                 // console.log("Friend list:", friend_list);
                 db_user.friend_list.push(thisID);
-                db_user.save(console.log);
+                db_user.save();
             }
             return save_try(friends, friend_list);
         };
@@ -700,7 +700,7 @@ exports.mixUpdate = function(req, res){
                             if (err) {
                                 console.log(err);
                             } else {
-                                console.log(mix);
+                                // console.log(mix);
                             }
                         });
                     }
